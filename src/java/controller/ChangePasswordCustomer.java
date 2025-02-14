595c73c2e33b47bc8385b81884a8de8a5b2eba33
takovn <hoatuyenuli2@gmail.com>
2025-02-14 11:14:03 +0700

    real fix bug 20

--- src/java/controller/ChangePasswordCustomer.java
+++ src/java/controller/ChangePasswordCustomer.java
@@ -38,7 +38,7 @@ public class ChangePasswordCustomer extends HttpServlet {
             String newPassword = request.getParameter("newPassword");
 
             if (id.isBlank() || oldPassword.isBlank() || newPassword.isBlank()) {
-                throw new Exception("All input must be filled!");
+                throw new IllegalArgumentException("All input must be filled!");
             }
 
             String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
